fee4bd2c6cc672e0f0b873a626522d5b4e53c268
ellensp <530024+ellensp@users.noreply.github.com>
2023-07-11 00:26:38 +1200

    add platformio support

--- rrd-glcd-test/rrd-glcd-test.ino
+++ rrd-glcd-test/rrd-glcd-test.ino
@@ -26,12 +26,12 @@ Sd2Card card;
 SdVolume volume;
 
 
-int x=0;                                //Offset postion of title  
+int x=0;                                //Offset postion of title
 int kill_pin_status = 1;                //Last read status of the stop pin, start at 1 to ensure buzzer is off
 int encoderPos = 1;                     //Current encoder position
 int encoder0PinALast;                   //Used to decode rotory encoder, last value
 int encoder0PinNow;                     //Used to decode rotory encoder, current value
-char posStr[4];                         //Char array to store encoderPos as a string  
+char posStr[4];                         //Char array to store encoderPos as a string
 char tmp_string[16];
 int enc_pin_status;                     //Last read status of the encoder button
 int sd_detect_pin_status = true;               //Last read status of the SD detect pin
@@ -52,7 +52,7 @@ unsigned long sdvolumecc;
 // SPI Com: SCK = en = 23, MOSI = rw = 17, CS = di = 16
 U8GLIB_ST7920_128X64_1X u8g(DOGLCD_SCK, DOGLCD_MOSI, DOGLCD_CS);
 
-void setup() {  
+void setup() {
   pinMode(SD_DETECT_PIN, INPUT);        // Set SD_DETECT_PIN as an unput
   digitalWrite(SD_DETECT_PIN, HIGH);    // turn on pullup resistors
   pinMode(KILL_PIN, INPUT);             // Set KILL_PIN as an unput
@@ -64,12 +64,12 @@ void setup() {
   pinMode(BTN_ENC, INPUT);              // Set BTN_ENC as an unput, encoder button
   digitalWrite(BTN_ENC, HIGH);          // turn on pullup resistors
   u8g.setFont(u8g_font_helvR08);        //Set the font for the display
-  u8g.setColorIndex(1);                 // Instructs the display to draw with a pixel on. 
+  u8g.setColorIndex(1);                 // Instructs the display to draw with a pixel on.
 }
 
 //Main arduino loop
 void loop() {
-  // Read the encoder and update encoderPos    
+  // Read the encoder and update encoderPos
   encoder0PinNow = digitalRead(BTN_EN1);
   if ((encoder0PinALast == LOW) && (encoder0PinNow == HIGH)) {
     if (digitalRead(BTN_EN2) == LOW) {
@@ -80,7 +80,7 @@ void loop() {
   }
   encoder0PinALast = encoder0PinNow;
 
-  
+
   //read in SD data when SD is incerted
   if (!sd_detect_pin_status && !gotsddata) {
       sdcardinit = card.init(SPI_HALF_SPEED, SDSS);
@@ -92,28 +92,28 @@ void loop() {
       gotsddata = true;
   }
 
-  //check if it is time to update the display 
+  //check if it is time to update the display
   currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
 
     //read the kill pin status
-    kill_pin_status = digitalRead(KILL_PIN); 
+    kill_pin_status = digitalRead(KILL_PIN);
     //read the encoder button status
     enc_pin_status = digitalRead(BTN_ENC);
-    //read the SD detect pin status  
+    //read the SD detect pin status
     sd_detect_pin_status = digitalRead(SD_DETECT_PIN);
     if (sd_detect_pin_status) {
       gotsddata = false;
     }
-  
+
     //Check if both Kill switch and encoder are pressed, if so switch on buzzer
-    if(kill_pin_status || enc_pin_status) digitalWrite(BEEPER_PIN, LOW); 
+    if(kill_pin_status || enc_pin_status) digitalWrite(BEEPER_PIN, LOW);
     else digitalWrite(BEEPER_PIN, HIGH);
 
     //Draw new screen
     u8g.firstPage();
-    do {  
+    do {
       if(gotsddata) draw2();
       else draw();
     } while( u8g.nextPage() );
@@ -125,12 +125,12 @@ void loop() {
   }
 }
 
-//Assemble the display  
+//Assemble the display
 void draw(){
   u8g.setColorIndex(0);
   u8g.drawBox(0,0,128,64);
   u8g.setColorIndex(1);
-  
+
   u8g.drawStr( 2+x, 10, "RRD GLCD TEST");
   u8g.drawStr( 2, 3*9, "Stop pin status:");
   if (kill_pin_status) u8g.drawStr( 84, 3*9, "Open");
@@ -143,12 +143,12 @@ void draw(){
   u8g.drawStr( 2, 6*9, "Encoder value:");
   sprintf (posStr, "%d", encoderPos);
   u8g.drawStr( 84, 6*9, posStr );
-  
+
   u8g.drawStr( 2, 5*9, "SD detect status:");
   if (sd_detect_pin_status) u8g.drawStr( 84, 5*9, "Open");
   else u8g.drawStr( 84, 5*9, "Closed");
-  
-  
+
+
   u8g.drawStr( 2, 7*9, "Buzzer:");
   if (kill_pin_status || enc_pin_status) u8g.drawStr( 84, 7*9, "Off");
   else u8g.drawStr( 84, 7*9, "On");
@@ -161,7 +161,7 @@ void draw2(){
   u8g.setColorIndex(0);
   u8g.drawBox(0,0,128,64);
   u8g.setColorIndex(1);
-  
+
   u8g.drawStr( 0, 1*10, "Initializing SD card:");
   if (sdcardinit) {
       u8g.drawStr( 90, 1*10, "OK");
@@ -193,7 +193,7 @@ void draw2(){
         volumesize *= sdvolumecc;       // we'll have a lot of clusters
         volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
         volumesize /= 1024;
-        
+
         u8g.drawStr( 0, 4*10,"Volume size (Mb):");
         itoa(volumesize, tmp_string, 10);
         u8g.drawStr( 90, 4*10,tmp_string);
